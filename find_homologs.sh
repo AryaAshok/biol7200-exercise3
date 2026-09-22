a7c03213aada11cdbe633ae0c81b2c7620002861
AryaAshok <54639371+AryaAshok@users.noreply.github.com>
2026-09-21 22:43:22 -0400

    final script

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+query_length=$(awk '!/^>/ {n += length($0)} END {print n}' "$1")
+
+tblastn -query "$1" -subject "$2" -outfmt 6 |
+awk -v qlen="$query_length" '$3 > 30 && $4 > 0.9 * qlen' > "$3"
+
+wc -l < "$3"
