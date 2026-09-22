6c389ba2b4776f97e529b1673566937fecfb3a6b
Arya Ashok <aryabashok@gmail.com>
2026-09-21 23:23:44 -0400

    fixed script

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-#another test for revert
 
-query_length=$(awk '!/^>/ {n += length($0)} END {print n}' "$1")
+query_length=$(awk '!/^>/ {gsub(/[[:space:]]/, ""); n += length($0)} END {print n}' "$1")
 
 tblastn -query "$1" -subject "$2" -outfmt 6 |
 awk -v qlen="$query_length" '$3 > 30 && $4 > 0.9 * qlen' > "$3"
